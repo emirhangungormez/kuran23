b21a1199652b1729f79b9dd2739b85683b716f0a
Emirhan Gungormez <emirhangungormez@users.noreply.github.com>
2026-03-09 03:13:00 +0300

    Refine shadda-style SVG icon and increase diacritics mark size

--- src/components/DiacriticsToggle.jsx
+++ src/components/DiacriticsToggle.jsx
@@ -13,7 +13,7 @@ export default function DiacriticsToggle({ enabled, onToggle, className = '' }) 
       title={label}
     >
       <svg className="diacritics-toggle-mark-only" viewBox="0 0 24 24" aria-hidden="true">
-        <path d="M4.5 14.5c1.1-2.8 3.2-2.8 4.3 0 1.1 2.8 3.2 2.8 4.3 0 1.1-2.8 3.2-2.8 4.4 0" />
+        <path d="M3.8 15.4c1.3-3.8 3-3.8 4.3 0 1.2 3.5 2.9 3.5 4.1 0 1.3-3.8 3-3.8 4.3 0 1.2 3.5 2.9 3.5 4.1 0" />
       </svg>
     </button>
   )
