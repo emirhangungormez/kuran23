8cc6cd2385928f21ffc2262307306d1ab007a57f
Emirhan Gungormez <emirhangungormez@users.noreply.github.com>
2026-03-14 12:38:40 +0300

    Refine library icon and footer logo

--- src/components/LibraryShelfIcon.jsx
+++ src/components/LibraryShelfIcon.jsx
@@ -10,10 +10,12 @@ export default function LibraryShelfIcon({ strokeWidth = 1.9, ...props }) {
       aria-hidden="true"
       {...props}
     >
-      <rect x="5" y="4" width="4" height="14" rx="1.2" />
-      <rect x="10" y="3" width="4" height="15" rx="1.2" />
-      <rect x="15" y="5" width="4" height="13" rx="1.2" />
-      <path d="M4 20h16" />
+      <path d="M4.5 7.5 9.25 5.2v13.2L4.5 20.7z" />
+      <path d="M9.25 5.2 14 7.5v13.2l-4.75-2.3z" />
+      <path d="M14 7.5 19.5 5v13.2L14 20.7z" />
+      <path d="M6.3 6.5 11 4.2" />
+      <path d="M10.8 8.7 15.7 6.4" />
+      <path d="M15.8 6.3 20 4.4" />
     </svg>
   )
 }
